3843f69175f163942d0411a69093ccdb0361ff2e
David Pfahler <pfahler.david@gmail.com>
2018-04-19 13:25:35 +0200

    added a Month Picker component widget to filter the flora for the monthh.

--- src/DSAMonthPicker.jsx
+++ src/DSAMonthPicker.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Select from "react-select";
-import { FormGroup, ControlLabel} from 'react-bootstrap';
+import { FormGroup, ControlLabel, ButtonToolbar, Button, ButtonGroup, Tooltip, OverlayTrigger} from 'react-bootstrap';
+import {DSAMonths} from './DSAUtils.jsx';
 
 export default class FilterWidget extends Component {
 
@@ -9,37 +9,61 @@ export default class FilterWidget extends Component {
   {
     super(props);
 
-    this.filter = this.filter.bind(this);
+    this.state = {
+      lang: "normal"
+    }
+
+    this.onToggle = this.onToggle.bind(this);
   }
 
-   filter(val) {
+   onToggle(event) {
     let filter = {};
+    const {property, selected} = this.props;
+    const v = Number(event.target.value);
 
-    if(val.constructor === Array)
-      filter[this.props.property] = val.map(function (option) {
-        return option.value;
-      });
-    else if(val.value)
-      filter[this.props.property] = val.value;
 
+    filter[property] = selected;
+    const index = selected.indexOf(v);
+
+    if(index === -1) {
+      filter[property].push(v);
+    }
+    else {
+      filter[property].splice(index, 1);
+    }
     this.props.onUserInput(filter);
-  }
-
-  getOptions() {
-    return ["Praios", "Hesinde"].map((m) => {return {value: m, label: m};});
   }
 
   render()
   {
-    let id = "filter-" + this.props.property;
+    const {selected, property, title} = this.props;
+    let id = "filter-" + property;
+
+    const renderMonths = DSAMonths.map((m, i) => {
+      const tooltip = (
+        <Tooltip id="tooltip">
+          Irdisch: <strong>{m.irdisch}</strong><br />
+          Thorwal: <strong>{m.thorwaler}</strong><br />
+          Zwerge: <strong>{m.zwerge}</strong>
+        </Tooltip>
+      )
+      const active = selected.length === 0 || selected.includes(i);
+      return (
+        <OverlayTrigger key={i} placement="bottom" overlay={tooltip}>
+          <Button value={i} active={active} onClick={this.onToggle}>
+              {m.normal}
+          </Button>
+        </OverlayTrigger>
+      );
+    });
     return (
       <FormGroup controlId={id}>
-        <ControlLabel>{this.props.title}</ControlLabel>
-        <Select
-          multi={true}
-          value={this.props.selected}
-          onChange={this.filter}
-          options={this.getOptions()} />
+        <ControlLabel>{title}</ControlLabel>
+        <ButtonToolbar>
+          <ButtonGroup type="checkbox">
+            {renderMonths}
+          </ButtonGroup>
+        </ButtonToolbar>
       </FormGroup>
     );
   }
